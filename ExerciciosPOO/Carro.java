74f6de8d59789c64b1f2e31832ceb7939a526903
brenneralexandria <obrenneralexandria@gmail.com>
2023-04-24 16:35:22 -0300

    Exercicios de POO

---
+++ ExerciciosPOO/Carro.java
@@ -0,0 +1,27 @@
+package ExerciciosPOO;
+
+public class Carro {
+    private String Marca;
+    private String Modelo;
+
+    public Carro(String marca, String modelo) {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+}
